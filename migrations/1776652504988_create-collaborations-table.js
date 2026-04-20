4a47cd99f1b3bb8451879754678d61b5202737b1
Rio Adrian <062340833237@student.polsri.ac.id>
2026-04-20 09:48:01 +0700

    feat: create collaborations table with foreign key constraints

---
+++ migrations/1776652504988_create-collaborations-table.js
@@ -0,0 +1,39 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.createTable('collaborations', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true
+        },
+        book_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+        user_id: {
+            type: 'VARCHAR(50)',
+            notNull: true
+        }
+    });
+
+    pgm.addConstraint('collaborations', 'unique_collaboration', 'UNIQUE(book_id, user_id)' );
+    pgm.addConstraint('collaborations', 'fk_collaborations.book_id_books.id', 'FOREIGN KEY(book_id) REFERENCES books(id) ON DELETE CASCADE');
+    pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropTable('collaborations');
+};
